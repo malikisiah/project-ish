78757fd98a26275207e21237ab0d2a9087218ebd
Malik <malikisiah214@gmail.com>
2025-01-25 16:16:09 -0600

    changed routes

---
+++ src/app/merch/page.tsx
@@ -0,0 +1,198 @@
+import Image from "next/image";
+
+const products1 = [
+  {
+    id: 1,
+    name: "Focus Paper Refill",
+    href: "merch/product/1",
+    price: "$13",
+    description: "3 sizes available",
+    imageSrc:
+      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-01.jpg",
+    imageAlt:
+      "Person using a pen to cross a task off a productivity paper card.",
+  },
+  {
+    id: 2,
+    name: "Focus Card Holder",
+    href: "product/2",
+    price: "$64",
+    description: "Walnut",
+    imageSrc:
+      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-02.jpg",
+    imageAlt: "Paper card sitting upright in walnut card holder on desk.",
+  },
+  {
+    id: 3,
+    name: "Focus Carry Pouch",
+    href: "product/3",
+    price: "$32",
+    description: "Heather Gray",
+    imageSrc:
+      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-03.jpg",
+    imageAlt:
+      "Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.",
+  },
+  // More products...
+];
+const products2 = [
+  {
+    id: 7,
+    name: "Electric Kettle",
+    href: "product/7",
+    price: "$149",
+    description: "Black",
+    imageSrc:
+      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-07.jpg",
+    imageAlt:
+      "Close up of long kettle spout pouring boiling water into pour-over coffee mug with frothy coffee.",
+  },
+  {
+    id: 8,
+    name: "Leather Workspace Pad",
+    href: "product/8",
+    price: "$165",
+    description: "Black",
+    imageSrc:
+      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-08.jpg",
+    imageAlt:
+      "Extra large black leather workspace pad on desk with computer, wooden shelf, desk organizer, and computer peripherals.",
+  },
+  {
+    id: 9,
+    name: "Leather Long Wallet",
+    href: "product/9",
+    price: "$118",
+    description: "Natural",
+    imageSrc:
+      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-09.jpg",
+    imageAlt:
+      "Leather long wallet held open with hand-stitched card dividers, full-length bill pocket, and simple tab closure.",
+  },
+  // More products...
+];
+
+export default function Page() {
+  return (
+    <>
+      <div>
+        <main>
+          <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="py-24 text-center">
+              <h1 className="text-4xl font-bold tracking-tight">
+                Be Creative Again With Boring Bundles©
+              </h1>
+              <p className="mx-auto mt-4 max-w-3xl text-base text-gray-500">
+                Are you a designer, copywriter, or creative? We get it - you
+                hate making proposals and admin documents. (We do too 🥱) That’s
+                why we created end-to-end templates to streamline your client
+                processes, giving you more time to focus on your creativity.
+              </p>
+            </div>
+
+            {/* Product grid */}
+            <section aria-labelledby="products-heading" className="mt-8">
+              <h2 id="products-heading" className="sr-only">
+                Products
+              </h2>
+
+              <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+                {products1.map((product) => (
+                  <a key={product.id} href={product.href} className="group">
+                    <Image
+                      width={600}
+                      height={600}
+                      alt={product.imageAlt}
+                      src={product.imageSrc}
+                      className="aspect-square w-full rounded-lg object-cover group-hover:opacity-75 sm:aspect-[2/3]"
+                    />
+                    <div className="mt-4 flex items-center justify-between text-base font-medium">
+                      <h3>{product.name}</h3>
+                      <p>{product.price}</p>
+                    </div>
+                    <p className="mt-1 text-sm italic text-gray-500">
+                      {product.description}
+                    </p>
+                  </a>
+                ))}
+              </div>
+            </section>
+
+            <section
+              aria-labelledby="featured-heading"
+              className="relative mt-16 overflow-hidden rounded-lg lg:h-96"
+            >
+              <div className="absolute inset-0">
+                <Image
+                  width={600}
+                  height={600}
+                  alt=""
+                  src="https://tailwindui.com/plus/img/ecommerce-images/category-page-01-featured-collection.jpg"
+                  className="size-full object-cover"
+                />
+              </div>
+              <div
+                aria-hidden="true"
+                className="relative h-96 w-full lg:hidden"
+              />
+              <div
+                aria-hidden="true"
+                className="relative h-32 w-full lg:hidden"
+              />
+              <div className="absolute inset-x-0 bottom-0 rounded-bl-lg rounded-br-lg bg-black/75 p-6 backdrop-blur backdrop-filter sm:flex sm:items-center sm:justify-between lg:inset-x-auto lg:inset-y-0 lg:w-96 lg:flex-col lg:items-start lg:rounded-br-none lg:rounded-tl-lg">
+                <div>
+                  <h2
+                    id="featured-heading"
+                    className="text-xl font-bold text-white"
+                  >
+                    Workspace Collection
+                  </h2>
+                  <p className="mt-1 text-sm text-gray-300">
+                    Upgrade your desk with objects that keep you organized and
+                    clear-minded.
+                  </p>
+                </div>
+                <a
+                  href="#"
+                  className="hover:/10 mt-6 flex shrink-0 items-center justify-center rounded-md border border-white/25 px-4 py-3 text-base font-medium text-white sm:ml-8 sm:mt-0 lg:ml-0 lg:w-full"
+                >
+                  View the collection
+                </a>
+              </div>
+            </section>
+
+            <section
+              aria-labelledby="more-products-heading"
+              className="mt-16 pb-24"
+            >
+              <h2 id="more-products-heading" className="sr-only">
+                More products
+              </h2>
+
+              <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+                {products2.map((product) => (
+                  <a key={product.id} href={product.href} className="group">
+                    <Image
+                      height={600}
+                      width={600}
+                      alt={product.imageAlt}
+                      src={product.imageSrc}
+                      className="aspect-square w-full rounded-lg object-cover group-hover:opacity-75 sm:aspect-[2/3]"
+                    />
+                    <div className="mt-4 flex items-center justify-between text-base font-medium">
+                      <h3>{product.name}</h3>
+                      <p>{product.price}</p>
+                    </div>
+                    <p className="mt-1 text-sm italic text-gray-500">
+                      {product.description}
+                    </p>
+                  </a>
+                ))}
+              </div>
+            </section>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,198 +1,125 @@
-import Image from "next/image";
+"use client";
 
-const products1 = [
-  {
-    id: 1,
-    name: "Focus Paper Refill",
-    href: "product/1",
-    price: "$13",
-    description: "3 sizes available",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-01.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    id: 2,
-    name: "Focus Card Holder",
-    href: "product/2",
-    price: "$64",
-    description: "Walnut",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-02.jpg",
-    imageAlt: "Paper card sitting upright in walnut card holder on desk.",
-  },
-  {
-    id: 3,
-    name: "Focus Carry Pouch",
-    href: "product/3",
-    price: "$32",
-    description: "Heather Gray",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-03.jpg",
-    imageAlt:
-      "Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.",
-  },
-  // More products...
-];
-const products2 = [
-  {
-    id: 7,
-    name: "Electric Kettle",
-    href: "product/7",
-    price: "$149",
-    description: "Black",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-07.jpg",
-    imageAlt:
-      "Close up of long kettle spout pouring boiling water into pour-over coffee mug with frothy coffee.",
-  },
-  {
-    id: 8,
-    name: "Leather Workspace Pad",
-    href: "product/8",
-    price: "$165",
-    description: "Black",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-08.jpg",
-    imageAlt:
-      "Extra large black leather workspace pad on desk with computer, wooden shelf, desk organizer, and computer peripherals.",
-  },
-  {
-    id: 9,
-    name: "Leather Long Wallet",
-    href: "product/9",
-    price: "$118",
-    description: "Natural",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-01-image-card-09.jpg",
-    imageAlt:
-      "Leather long wallet held open with hand-stitched card dividers, full-length bill pocket, and simple tab closure.",
-  },
-  // More products...
+import { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
 ];
 
-export default function Page() {
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
-    <>
-      <div>
-        <main>
-          <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="py-24 text-center">
-              <h1 className="text-4xl font-bold tracking-tight">
-                Be Creative Again With Boring Bundles©
-              </h1>
-              <p className="mx-auto mt-4 max-w-3xl text-base text-gray-500">
-                Are you a designer, copywriter, or creative? We get it - you
-                hate making proposals and admin documents. (We do too 🥱) That’s
-                why we created end-to-end templates to streamline your client
-                processes, giving you more time to focus on your creativity.
-              </p>
-            </div>
-
-            {/* Product grid */}
-            <section aria-labelledby="products-heading" className="mt-8">
-              <h2 id="products-heading" className="sr-only">
-                Products
-              </h2>
-
-              <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-                {products1.map((product) => (
-                  <a key={product.id} href={product.href} className="group">
-                    <Image
-                      width={600}
-                      height={600}
-                      alt={product.imageAlt}
-                      src={product.imageSrc}
-                      className="aspect-square w-full rounded-lg object-cover group-hover:opacity-75 sm:aspect-[2/3]"
-                    />
-                    <div className="mt-4 flex items-center justify-between text-base font-medium">
-                      <h3>{product.name}</h3>
-                      <p>{product.price}</p>
-                    </div>
-                    <p className="mt-1 text-sm italic text-gray-500">
-                      {product.description}
-                    </p>
-                  </a>
-                ))}
-              </div>
-            </section>
-
-            <section
-              aria-labelledby="featured-heading"
-              className="relative mt-16 overflow-hidden rounded-lg lg:h-96"
-            >
-              <div className="absolute inset-0">
-                <Image
-                  width={600}
-                  height={600}
+    <div className="min-h-screen bg-white">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <Dialog
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+          className="lg:hidden"
+        >
+          <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
                   alt=""
-                  src="https://tailwindui.com/plus/img/ecommerce-images/category-page-01-featured-collection.jpg"
-                  className="size-full object-cover"
+                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                  className="h-8 w-auto"
                 />
-              </div>
-              <div
-                aria-hidden="true"
-                className="relative h-96 w-full lg:hidden"
-              />
-              <div
-                aria-hidden="true"
-                className="relative h-32 w-full lg:hidden"
-              />
-              <div className="absolute inset-x-0 bottom-0 rounded-bl-lg rounded-br-lg bg-black/75 p-6 backdrop-blur backdrop-filter sm:flex sm:items-center sm:justify-between lg:inset-x-auto lg:inset-y-0 lg:w-96 lg:flex-col lg:items-start lg:rounded-br-none lg:rounded-tl-lg">
-                <div>
-                  <h2
-                    id="featured-heading"
-                    className="text-xl font-bold text-white"
-                  >
-                    Workspace Collection
-                  </h2>
-                  <p className="mt-1 text-sm text-gray-300">
-                    Upgrade your desk with objects that keep you organized and
-                    clear-minded.
-                  </p>
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
                 </div>
-                <a
-                  href="#"
-                  className="hover:/10 mt-6 flex shrink-0 items-center justify-center rounded-md border border-white/25 px-4 py-3 text-base font-medium text-white sm:ml-8 sm:mt-0 lg:ml-0 lg:w-full"
-                >
-                  View the collection
-                </a>
-              </div>
-            </section>
-
-            <section
-              aria-labelledby="more-products-heading"
-              className="mt-16 pb-24"
-            >
-              <h2 id="more-products-heading" className="sr-only">
-                More products
-              </h2>
-
-              <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-                {products2.map((product) => (
-                  <a key={product.id} href={product.href} className="group">
-                    <Image
-                      height={600}
-                      width={600}
-                      alt={product.imageAlt}
-                      src={product.imageSrc}
-                      className="aspect-square w-full rounded-lg object-cover group-hover:opacity-75 sm:aspect-[2/3]"
-                    />
-                    <div className="mt-4 flex items-center justify-between text-base font-medium">
-                      <h3>{product.name}</h3>
-                      <p>{product.price}</p>
-                    </div>
-                    <p className="mt-1 text-sm italic text-gray-500">
-                      {product.description}
-                    </p>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Log in
                   </a>
-                ))}
+                </div>
               </div>
-            </section>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </header>
+
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
+          <div className="flex items-center justify-center gap-12 text-center">
+            <Link
+              href="/music"
+              className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl"
+            >
+              MUSIC
+            </Link>
+            <Link
+              href="/media"
+              className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl"
+            >
+              MEDIA
+            </Link>
+            <Link
+              href="/merch"
+              className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl"
+            >
+              MERCH
+            </Link>
           </div>
-        </main>
+        </div>
+        <p className="text-center">Hello World</p>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
